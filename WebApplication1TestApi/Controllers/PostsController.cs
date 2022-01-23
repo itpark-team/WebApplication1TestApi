166228dc21994e41ed8c1f275dac5685a1edbbc0
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-01-23 19:11:00 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1TestApi/Controllers/PostsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1TestApi.Models;
+
+namespace WebApplication1TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public List<PostData> Get()
+        {
+            return PostDatasRepository.postDatas;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]PostData postData)
+        {
+            PostDatasRepository.postDatas.Add(postData);
+        }
+    }
+}
